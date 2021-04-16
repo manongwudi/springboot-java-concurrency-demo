7ec7c01f18fbc35ad0fb8b645bb54dbf482f6f08
jiangqiao <jiangqiao@mafengwo.com>
2021-04-16 20:00:44 +0800

    测试工程

---
+++ src/main/java/com/wudimanong/concurrent/ConcurrencyDemo.java
@@ -0,0 +1,15 @@
+package com.wudimanong.concurrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wudimanong
+ */
+@SpringBootApplication
+public class ConcurrencyDemo {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrencyDemo.class, args);
+    }
+}
---
+++ src/test/java/com/wudimanong/concurrent/CyclicBarrierTest.java
@@ -0,0 +1,19 @@
+package com.wudimanong.concurrent;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author jiangqiao
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+public class CyclicBarrierTest {
+
+    @Test
+    public void cyclicBarrierTest() {
+        System.out.println("测试下用法");
+    }
+}
